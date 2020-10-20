b3ea92542447050336b860102bf4d4b2365141d9
Conor ONeill <conor@conoroneill.com>
2020-10-20 06:11:52 +0100

    video thumbnails added. Publishing on GitHub

--- README.md
+++ README.md
@@ -2,24 +2,40 @@
 
 A Serverless Lambda function in Node.js to generate a proper RSS feed for any channel on Vimeo, now that they have decided to not support the open web any more.
 
-Installation:
+## TODO
+* More error handling
+
+## Installation
 
 ```bash
 npm install -g serverless
 git clone git@github.com:conoro/vimeo-rss.git
 cd vimeo-rss
 npm install
+```
+
+* Go to https://developer.vimeo.com/, login and create an App for yourself only
+* The Free Tier is fine
+* Then create an Access Token with the option "Authenticated (You)"
+* copy config-sample.js to config.js
+* Paste the access token into the relevant place in config.js
+* Then deploy:
+
+```bash
 serverless deploy
 ```
 
-Then subscribe in your RSS reader to a URL like:
+Then subscribe to each Vimeo user in your RSS reader (like Feedly) using a URL like:
 
 ```
-https://your-URL-for-your-serverless-function/dev/?page=https://vimeo.com/camp4collective/videos
+https://your-URL-for-your-serverless-function/dev/rss?user=alhumphreys
 
 ```
 
 (Assumes you have your AWS config already setup)
+
+## Notes
+* I used https://opml-gen.ovh/ to bulk import a big list of Vimeo user accounts in one go as an OPML file into Feedly
 
 LICENSE Apache-2.0
 
--- index.js
+++ index.js
@@ -1,93 +1,61 @@
 // vimeo-rss - Copyright Conor O'Neill 2020, conor@conoroneill.com
 // LICENSE Apache-2.0
-// Invoke like https://url.of.serverless.function/dev/rss?page=https://vimeo.com/camp4collective
+// Invoke like https://url.of.serverless.function/dev/rss?user=camp4collective
 
-/*
+
 module.exports.check = (event, context, callback) => {
-*/
 
-async function getVimeoVideos() {
-    var config = require('./config');
-    const axios = require('axios');
-
-    try {
-        const response = await axios.get("https://api.vimeo.com/users/48632997/videos", { headers: {"Authorization" : `bearer ${config.vimeoToken}`} });
-        console.dir(response);
-    } catch (error) {
-        // handle error
-        console.log("Error 1: ", error);
-    }
-
-}
-
-getVimeoVideos();
-
-/*
-    var RSS = require("rss");
-    var sectionURL = event.query.page;
-    // var sectionURL = "https://vimeo.com/camp4collective/videos";
-
-
-    var feed = new RSS({
-        title: sectionURL.split('/').slice(-2)[0] + " on Vimeo RSS",
-        description: "Vimeo videos for " + sectionURL.split('/').slice(-2)[0],
-        feed_url: "http://example.com/rss.xml",
-        site_url: sectionURL,
-        image_url: "https://prowly-uploads.s3.eu-west-1.amazonaws.com/uploads/press_rooms/company_logos/563/indeks.png",
-        docs: "http://example.com/rss/docs.html",
-        managingEditor: "conor@conoroneill.com",
-        webMaster: "conor@conoroneill.com",
-        copyright: "2020 Conor ONeill",
-        language: "en",
-        pubDate: "Sep 14, 2020 08:00:00 GMT",
-        ttl: "60"
-    });
-
-    doEverything();
-
-    async function doEverything() {
+    async function getVimeoVideos() {
+        var config = require('./config');
+        const axios = require('axios');
+        var RSS = require("rss");
+        var user = event.query.user;
 
         try {
-            browser = await chromium.puppeteer.launch({
-                args: chromium.args,
-                defaultViewport: chromium.defaultViewport,
-                executablePath: await chromium.executablePath,
-                headless: chromium.headless,
-                ignoreHTTPSErrors: true,
+            const response = await axios.get("https://api.vimeo.com/users/" + user + "/videos", { headers: { "Authorization": `bearer ${config.vimeoToken}` } });
+            var feed = new RSS({
+                title: response.data.data[0].user.name + " on Vimeo RSS",
+                description: response.data.data[0].user.short_bio,
+                feed_url: "http://example.com/rss.xml",
+                site_url: "https://vimeo.com/" + user,
+                image_url: "https://raw.githubusercontent.com/conoro/vimeo-rss/master/vimeo-square.png",
+                docs: "http://example.com/rss/docs.html",
+                managingEditor: "conor@conoroneill.com",
+                webMaster: "conor@conoroneill.com",
+                copyright: "2020 Conor ONeill",
+                language: "en",
+                pubDate: "Oct 20, 2020 05:00:00 GMT",
+                ttl: "60"
             });
-            const page = await browser.newPage();
-            await page.setViewport({ width: 1000, height: 926 });
-            await page.goto(sectionURL, { waitUntil: 'networkidle2' });
 
-            let bodyHTML = await page.evaluate(() => document.body.innerHTML);
-
-            let $ = cheerio.load(bodyHTML);
-
-            $(".js-browse_list li").each(function () {
-                let title = $(this).find("a").attr("title").trim();
-                var link = "https://www.vimeo.com" + $(this).find("a").attr("href");
-                // console.log(title, "https://www.vimeo.com" + link);
-                // let img = "https://prowly-uploads.s3.eu-west-1.amazonaws.com/uploads/press_rooms/company_logos/563/indeks.png";
-                let description = title;
-                articleDate = new Date();
+            for (i = 0; i < response.data.data.length; i++) {
+                var images = response.data.data[i].pictures.sizes;
+                var img = images[images.length-4].link;
+                if (typeof img === 'undefined' || img === null) img = "https://raw.githubusercontent.com/conoro/vimeo-rss/master/vimeo-square.png";
+                  var summary = response.data.data[i].description;
+                  if (typeof summary === 'undefined' || summary === null) summary = "No Summary Found";
+                  var description = '<img src="' + img + '" alt="' + response.data.data[i].name + '" /> ' + summary;
+        
                 feed.item({
-                    title: title,
+                    title: response.data.data[i].name,
                     description: description,
-                    url: link,
-                    author: "vimeo@example.com",
-                    date: articleDate.toUTCString()
+                    url: response.data.data[i].link,
+                    author: response.data.data[i].user.name,
+                    date: response.data.data[i].created_time
                 });
-            });
+            }
 
-            browser.close()
             var xml = feed.xml();
-            // console.log(xml);
             context.succeed(xml);
-        } catch (err) {
-            console.log(err)
-            browser.close();
+
+        } catch (error) {
+            // handle error
+            console.log("Error 1: ", error);
         }
+
     }
+
+    getVimeoVideos();
+
 };
 
-*/
