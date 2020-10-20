2a698a06a7726d24817e97d58693f63a7846c21b
Conor ONeill <conor@conoroneill.com>
2020-10-20 06:14:57 +0100

    added missing example config file

---
+++ config-sample.js
@@ -0,0 +1,11 @@
+// Go to https://developer.vimeo.com/, login and create an App for yourself only
+// Free Tier is fine
+// Then create an Access Token with the option "Authenticated (You)"
+// Paste that token below and rename this file to config.js
+
+var config = {};
+config.vimeoToken = "AccessTokenFromVimeo"
+
+
+module.exports = config;
+
